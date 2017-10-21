e5af95286a93b0888a99c859098686eba50a3636
Michael Scheurer <michael.scheurer@seccom.ch>
2017-10-21 11:22:34 +0200

    dashboard responsive

--- src/components/admin/dashboard.vue
+++ src/components/admin/dashboard.vue
@@ -4,27 +4,29 @@
       <div slot="title">
         Admin Dashboard
       </div>
-      <div class="column">
-        <a href="#/admin/users">
-          <div class="notification is-warning subtitle is-4">
-            Users
-          </div>
-        </a>
-      </div>
-      <div class="column">
-        <a href="#/admin/machines">
-          <div class="notification is-primary subtitle is-4">Machines</div>
-        </a>
-      </div>
-      <div class="column">
-        <a href="#/admin/statistics">
-          <div class="notification is-success subtitle is-4">Statistics</div>
-        </a>
-      </div>
-      <div class="column">
-        <a href="#/admin/invoices">
-          <div class="notification is-danger subtitle is-4">Invoices</div>
-        </a>
+      <div class="columns">
+        <div class="column">
+          <a href="#/admin/users">
+            <div class="notification is-warning subtitle is-4">
+              Users
+            </div>
+          </a>
+        </div>
+        <div class="column">
+          <a href="#/admin/machines" >
+            <div class="notification is-primary subtitle is-4">Machines</div>
+          </a>
+        </div>
+        <div class="column">
+          <a href="#/admin/statistics">
+            <div class="notification is-success subtitle is-4">Statistics</div>
+          </a>
+        </div>
+        <div class="column">
+          <a href="#/admin/invoices">
+            <div class="notification is-danger subtitle is-4">Invoices</div>
+          </a>
+        </div>
       </div>
     </dashboard>
   </div>
@@ -58,6 +60,10 @@ export default {
 
 .panel {
   padding-top: 10vh;
+}
+
+.columns {
+  width: 100%
 }
 
 </style>
